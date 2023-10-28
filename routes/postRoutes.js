ba1783511b2bbd3db0c8009bdfd0cf822e5ffa43
thisisrahulpal <87530955+thisisrahulpal@users.noreply.github.com>
2023-10-28 12:41:30 +0530

    Http method changed

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ router.get("/latest", require("../controllers/Post/latestPosts").process); // Ge
 router.get("/", require("../controllers/Post/getPost").process); // Get all posts
 router.get("/:id", require("../controllers/Post/getSinglePost").process); // Get single post
 router.post("/", require("../controllers/Post/createPost").process); // Create post
-router.post("/:id", require("../controllers/Post/deletePost").process); // Delete post
+router.delete("/:id", require("../controllers/Post/deletePost").process); // Delete post
 router.put("/:id", require("../controllers/Post/updatePost").process); // Update post
 
 module.exports = router;
